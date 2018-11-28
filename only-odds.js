218d755aff3587fb074862f84f35a2a5670237b1
Miyuki Abe <miyuki0809@gmail.com>
2018-11-27 23:36:06 -0500

    Testing Git. Did not update Doc

---
+++ only-odds.js
@@ -0,0 +1,19 @@
+// Define a function, `onlyOdds`, that accepts a number as an argument. `onlyOdds`
+// should should return the sum of all the odd numbers between the given number
+// and 1.
+
+// If `onlyOdds` receives an argument less than 1, it should return 0.
+
+// ```javascript
+// onlyOdds(6); // => 9 (5 + 3 + 1)
+// ```
+
+// YOUR CODE BELOW
+
+    function onlyOdds(aNum) {
+        let sum = 0;
+        for (let i = 1; i <= aNum; i++){
+        if (i % 2 === 1) sum+= i 
+        }
+        return sum;
+    }
